c26b8b4ebef0d3b2248285a6d6aa02d167b788f4
Nikita <121949031+SEADRAGON27@users.noreply.github.com>
2024-07-14 10:45:15 +0300

    Update OAuth2.config.ts

--- src/configs/OAuth2.config.ts
+++ src/configs/OAuth2.config.ts
@@ -1,5 +1,5 @@
 export const oauthConfig = {
   clientID: process.env.OAUTH_CLIENT_ID,
   clientSecret: process.env.OAUTH_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/callback/google',
+  callbackURL: process.env.CALLBACK_URL,
 };
